95e2354106e2decb823a6a8e25a56bd74f57e40a
JKtrigger <kasher89@mail.ru>
2014-12-22 16:55:09 +0300

    Update tests.py

--- verification/tests.py
+++ verification/tests.py
@@ -1,1 +1,53 @@
-TESTS = ''
+TESTS = {
+  "Basics": [
+    {
+    "input": (['Norwegian-Dunhill','Marlboro-blue','Brit-3',
+              'German-coffee','beer-white','cat-water',
+              'horse-2','milk-3','4-Rothmans',
+              'dog-Swede','Norwegian-1','horse-Marlboro',
+              'bird-Brit','4-green','Winfield-beer',
+              'Dane-blue','5-dog','blue-horse',
+              'yellow-cat','Winfield-Swede','tea-Marlboro'],['hourse-cigarettes'])
+    "answer": 'Marlboro'
+    },
+    {
+    "input": (['Norwegian-Dunhill','Marlboro-blue','Brit-3',
+              'German-coffee','beer-white','cat-water',
+              'horse-2','milk-3','4-Rothmans',
+              'dog-Swede','Norwegian-1','horse-Marlboro',
+              'bird-Brit','4-green','Winfield-beer',
+              'Dane-blue','5-dog','blue-horse',
+              'yellow-cat','Winfield-Swede','tea-Marlboro'],['green-num'])
+    "answer": '4' 
+    },
+    {
+    "input": (['Norwegian-Dunhill','Marlboro-blue','Brit-3',
+              'German-coffee','beer-white','cat-water',
+              'horse-2','milk-3','4-Rothmans',
+              'dog-Swede','Norwegian-1','horse-Marlboro',
+              'bird-Brit','4-green','Winfield-beer',
+              'Dane-blue','5-dog','blue-horse',
+              'yellow-cat','Winfield-Swede','tea-Marlboro'],['milk-colors'])
+    "answer": 'red'
+    },        
+    {
+    "input": (['Norwegian-Dunhill','Marlboro-blue','Brit-3',
+              'German-coffee','beer-white','cat-water',
+              'horse-2','milk-3','4-Rothmans',
+              'dog-Swede','Norwegian-1','horse-Marlboro',
+              'bird-Brit','4-green','Winfield-beer',
+              'Dane-blue','5-dog','blue-horse',
+              'yellow-cat','Winfield-Swede','tea-Marlboro'],['Winfield-person'])
+    "answer": 'Swede'
+    },
+    {
+    "input": (['Norwegian-Dunhill','Marlboro-blue','Brit-3',
+              'German-coffee','beer-white','cat-water',
+              'horse-2','milk-3','4-Rothmans',
+              'dog-Swede','Norwegian-1','horse-Marlboro',
+              'bird-Brit','4-green','Winfield-beer',
+              'Dane-blue','5-dog','blue-horse',
+              'yellow-cat','Winfield-Swede','tea-Marlboro'],['yellow-pet'])
+    "answer": 'cat'
+    },
+            ]
