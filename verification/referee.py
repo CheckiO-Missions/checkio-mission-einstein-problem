ea21ed22cc2f9456dd4d11ae1af922c986b0945f
JKtrigger <kasher89@mail.ru>
2014-12-24 16:30:00 +0200

    Update referee.py

--- verification/referee.py
+++ verification/referee.py
@@ -1,18 +1,20 @@
 from checkio.signals import ON_CONNECT
 from checkio import api
-from checkio.referees.code import CheckiORefereeCode
+from checkio.referees.io import CheckiOReferee
 from checkio.referees import cover_codes
 from checkio.referees import checkers
 
 from tests import TESTS
-def checker(result, test_data):
-    return result['code_result']
 
 api.add_listener(
     ON_CONNECT,
-    CheckiORefereeCode(
+    CheckiOReferee(
         tests=TESTS,
-        check_result=checker,
+        cover_code={
+            'python-27': cover_codes.unwrap_args,  # or None
+            'python-3': cover_codes.unwrap_args
+        },
+        # checker=None,  # checkers.float.comparison(2)
         # add_allowed_modules=[],
         # add_close_builtins=[],
         # remove_allowed_modules=[]
