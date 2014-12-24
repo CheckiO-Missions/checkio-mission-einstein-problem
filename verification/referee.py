53c388da3ae34080854a06e3f3fee76595ea3f00
JKtrigger <kasher89@mail.ru>
2014-12-24 16:36:00 +0200

    Update referee.py

--- verification/referee.py
+++ verification/referee.py
@@ -1,16 +1,9 @@
 from checkio.signals import ON_CONNECT
 from checkio import api
 from checkio.referees.io import CheckiOReferee
-from checkio.referees import cover_codes
-from checkio.referees import checkers
 
 from tests import TESTS
-def checker(result, test_data):
-    return result['code_result']
-    
+
 api.add_listener(
     ON_CONNECT,
-    CheckiOReferee(
-        tests=TESTS,
-        check_result=checker,
-    ).on_ready)
+    CheckiOReferee(tests=TESTS).on_ready)
